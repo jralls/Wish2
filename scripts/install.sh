82ae333d573ee291fbd3a7f5fc51e9e00bd575bf
root <empty>
2002-12-04 02:36:57 +0000

    Initial revision

---
+++ scripts/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# chkconfig: - 99 99
+# description: Starts and stops the X10 control interface \
+#
+ver=`uname -r`
+moduledir="/lib/modules/$ver/kernel/drivers/char/x10"
+
+if [ ! -f x10.o ]
+then 
+	echo "You must make the objects first.  Run make"
+	exit 1
+fi
+
+if [ ! -d $moduledir ]
+then
+	echo "mkdir $moduledir"
+	mkdir $moduledir
+fi
+
+echo "cp -f x10attach /usr/sbin/"
+cp -f x10attach /usr/sbin/
+echo "cp -f *.o $moduledir"
+cp -f *.o $moduledir
+echo "depmod -a $ver"
+depmod -a $ver
+
