5d2f07d151f90159953a6fba8a1f3045f0fd1ff1
whiles <empty>
2003-01-12 23:35:57 +0000

    added x10_plusb.o

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,8 +14,8 @@ echo "cp -f utils/x10attach /usr/bin/"
 cp -f utils/x10attach /usr/bin/
 echo "cp -f utils/nbread /usr/bin/"
 cp -f utils/nbread /usr/bin/
-echo "cp -f x10_pl.o x10_cm11a.o x10_cm17a.o $moduledir"
-cp -f x10_pl.o x10_cm11a.o x10_cm17a.o $moduledir
+echo "cp -f x10_pl.o x10_cm11a.o x10_cm17a.o x10_plusb.o $moduledir"
+cp -f x10_pl.o x10_cm11a.o x10_cm17a.o x10_plusb.o $moduledir
 echo "depmod -a $ver"
 depmod -a $ver
 
