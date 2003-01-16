736da080078632f92e52af60cc87935e56f76e7d
whiles <empty>
2003-01-16 21:28:54 +0000

    moved x10attach to /usr/sbin added x10logd to /usr/sbin

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,8 +10,16 @@ then
 	mkdir $moduledir
 fi
 
-echo "cp -f utils/x10attach /usr/bin/"
-cp -f utils/x10attach /usr/bin/
+# earlier versions of this script put x10attach in /usr/bin
+if [ -f /usr/bin/x10attach ]
+then
+	rm -f /usr/bin/x10attach
+fi
+
+echo "cp -f utils/x10attach /usr/sbin/"
+cp -f utils/x10attach /usr/sbin/
+echo "cp -f utils/x10logd /usr/sbin/"
+cp -f utils/x10logd /usr/sbin/
 echo "cp -f utils/nbread /usr/bin/"
 cp -f utils/nbread /usr/bin/
 echo "cp -f x10_pl.o x10_cm11a.o x10_cm17a.o x10_plusb.o $moduledir"
