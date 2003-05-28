381f3cae8fe9bb48d441241dfb9b369c1f7feb9a
whiles <empty>
2003-05-28 16:42:59 +0000

    updated to accept the kernel directory

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,14 @@
 #
 #
 ver=`uname -r`
+if [ $# -ne 0 ]
+then
+pwd=`dirname $0`
+ver=`$pwd/getversion.sh "$1"`
+fi
 moduledir="/lib/modules/$ver/kernel/drivers/char/x10"
+
+echo "Installing objects in $ver"
 
 if [ ! -d $moduledir ]
 then
@@ -16,6 +23,10 @@ then
 	rm -f /usr/bin/x10attach
 fi
 
+echo "cp -f example_scripts/x10.cm11a.sh /usr/local/etc/x10_cm11a"
+echo "cp -f example_scripts/x10.cm17a.sh /usr/local/etc/x10_cm17a"
+echo "cp -f example_scripts/x10.plusb.sh /usr/local/etc/x10_plusb"
+echo "cp -f example_scripts/x10.pl.sh /usr/local/etc/x10_pl"
 echo "cp -f utils/x10attach /usr/sbin/"
 cp -f utils/x10attach /usr/sbin/
 echo "cp -f utils/x10logd /usr/sbin/"
