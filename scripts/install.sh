2ce3e3c9063cd91ef2f955da6d4d732976465d4e
whiles <empty>
2003-05-30 03:13:36 +0000

    added "-e" option to depmod

--- scripts/install.sh
+++ scripts/install.sh
@@ -37,6 +37,6 @@ echo "cp -f utils/x10watch /usr/bin/"
 cp -f utils/x10watch /usr/bin/
 echo "cp -f x10_pl.o x10_cm11a.o x10_cm17a.o x10_plusb.o $moduledir"
 cp -f x10_pl.o x10_cm11a.o x10_cm17a.o x10_plusb.o $moduledir
-echo "depmod -a $ver"
-depmod -a $ver
+echo "depmod -ae $ver"
+depmod -ae $ver
 
