d94dba712c099aed37dd629646811be485b56a5f
whiles <empty>
2004-01-04 19:37:04 +0000

    all functionality implemented in dev.c/h

--- dev/x10.h
+++ dev/x10.h
@@ -4,17 +4,18 @@
 #include <linux/types.h>
 
 #define MESSAGE_QUEUE_SIZE	16
+#define MAX_LOG 		32
 
 typedef struct x10message {
-  __u8		type;		// COMMAND=1, DATA=2
+  __u8		source;		// Where did the message come from/go to
   __u8		housecode;	// 0-16
   __u8		unitcode;	// 0-16
   __u8		command;	// see X10_CMD_* below
   __u32		flag;		// flags (see below)
 } x10_message_t;
 
-// Values for the type field
-#define X10_CMD			0x01
+// Values for the source field
+#define X10_CONTROL		0x01
 #define X10_DATA		0x02
 #define X10_API			0x04
 
@@ -35,6 +36,7 @@ typedef struct x10message {
 #define X10_CMD_STATUSOFF       0xd
 #define X10_CMD_STATUS          0xe
 #define X10_CMD_HAILACKNOWLEDGE 0xf
+#define X10_CMD_END		0x10
 
 // Values for flag field
 #define X10_FLAG_LLSEEK		0x10	// flag to contain seek info
