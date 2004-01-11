a934b4a6a45de664c539a70e5567f6aae5680748
whiles <empty>
2004-01-11 20:00:51 +0000

    threads and communications set up but untested

--- daemons/main.c
+++ daemons/main.c
@@ -1,6 +1,6 @@
 /*
  *
- * $Id: x10d.c,v 1.3 2004/01/06 03:45:51 whiles Exp whiles $
+ * $Id: x10d.c,v 1.4 2004/01/10 19:31:03 whiles Exp whiles $
  *
  * Copyright (c) 2002 Scott Hiles
  *
@@ -45,10 +45,13 @@
 #include <sys/wait.h>
 #include <setjmp.h>
 #include <syslog.h>
+#include <sched.h>
 #include <libgen.h>
 #include <asm/types.h>
+#include <semaphore.h>
 
-#include "../x10.h"
+#include "../include/x10.h"
+#include "../fs/strings.h"
 #include "x10d.h"
 
 #define sighandler_type void
@@ -63,6 +66,9 @@ static char *logtag;				// tag to add to log lines
 static int syslog_facility=LOG_LOCAL5;
 static int api;					// file handle for the API interface
 static int timeout=10;				// timeout for waiting for response from API
+static int xmitpid=0;				// pid for transmitter thread
+static char *serial=NULL;			// serial device
+static void **xmitstack = NULL;			// stack space for transmit thread
 
 char localhostname[MAXHOSTNAMELEN+1];
 char *localdomain;
@@ -75,12 +81,26 @@ static sighandler_type die(int sig),reapchild();
 static void syntax(int argc, char *argv[],int i);
 static void write_log(int fd,char *buf,int len);
 static void start();
+static void listen();
+static int update_state(int dir, int hc, int uc, int fc,unsigned char *buf, int len);
+
+static struct state {
+  int mode[MAX_HOUSECODES];
+  unsigned char status[MAX_HOUSECODES][MAX_UNITS];
+  int hc;
+  int uc[MAX_HOUSECODES][MAX_UNITS];
+  int lastuc;
+  int fcsent[MAX_UNITS];
+  int fc;
+  sem_t lock;
+} state;
 
 int main(int argc,char *argv[])
 {
   register int i;
   char *arg;
 
+  memset(&state,0,sizeof(state));
   progname = basename(argv[0]);
   logtag = progname;
   for (i = 1; i < argc; i++) {
@@ -100,6 +120,9 @@ int main(int argc,char *argv[])
     } else if (!strcasecmp(&arg[1],"tag")) {
       TESTNEXTARG;
       logtag = argv[++i];
+    } else if (!strcasecmp(&arg[1],"device")) {
+      TESTNEXTARG;
+      serial = argv[++i];
     } else if (!strcasecmp(&arg[1],"timeout")) {
       TESTNEXTARG;
       timeout = atoi(argv[++i]);
@@ -107,6 +130,10 @@ int main(int argc,char *argv[])
       syntax(argc,argv,i);
       exit (-1);
     }
+  }
+  if (serial == NULL) {
+    dprintf("No device specified.  Must have -device <device> present\n");
+    exit (-1);
   }
   start();
   return -1;
@@ -117,14 +144,11 @@ void start()
   FILE *fp;
   register char *p;
   struct sigaction action;
-  x10_message_t m;
   int ret,connected = 0,i;
 
   // first open up the syslog
   openlog(logtag,LOG_NDELAY | LOG_PID,syslog_facility);
 
-  // open our API device
-//  api = open(device,O_RDWR | O_NONBLOCK);
   api = open(device,O_RDWR);
   if (api < 0) {
     char obuffer[256];
@@ -135,24 +159,28 @@ void start()
   }
   syslog(LOG_INFO,"starting. api-device=%s, pidfile=%s\n",device,pidfile);
   dprintf("%s starting\ndevice=%s\npidfile=%s\nlogtag=%s\n",progname,device,pidfile,logtag);
-  memset(&m,0,sizeof(m));
-  m.source = X10_API;
-  m.command = X10_CMD_STATUS;
-  ret = write(api,&m,sizeof(m));
-  if (ret < sizeof(m)) {
-    syslog(LOG_INFO,"Error:  Unable to write to X10 device driver\n");
-    exit(-1);
-  }
-  for (i = 0; i < timeout; i++) {
+  // this could be a real function to do the read/write, but I did a null for quick and dirty test
+  {
+    x10_message_t m;
     memset(&m,0,sizeof(m));
-    if (read(api,&m,sizeof(m)) != sizeof(m))
-      syslog(LOG_INFO,"Error:  Incorrect byte count read from %s\n",device);
-    if (m.source == X10_API && m.command == X10_CMD_ON) {
-      syslog(LOG_INFO,"Successfully opened X10 API device %s\n",device);
-      connected = 1;
-      break;
+    m.source = X10_API;
+    m.command = X10_CMD_STATUS;
+    ret = write(api,&m,sizeof(m));
+    if (ret < sizeof(m)) {
+      syslog(LOG_INFO,"Error:  Unable to write to X10 device driver\n");
+      exit(-1);
     }
-    syslog(LOG_INFO,"X10 message: src=0x%x, hc=0x%x, uc=0x%x, cmd=0x%x, f=0x%x\n",m.source,m.housecode,m.unitcode,m.command,m.flag);
+    for (i = 0; i < timeout; i++) {
+      memset(&m,0,sizeof(m));
+      if (read(api,&m,sizeof(m)) != sizeof(m))
+        syslog(LOG_INFO,"Error:  Incorrect byte count read from %s\n",device);
+      if (m.source == X10_API && m.command == X10_CMD_ON) {
+        syslog(LOG_INFO,"Successfully opened X10 API device %s\n",device);
+        connected = 1;
+        break;
+      }
+    }
+    dprintf("X10 message: src=0x%x, hc=0x%x, uc=0x%x, cmd=0x%x, f=0x%x\n",m.source,m.housecode,m.unitcode,m.command,m.flag);
     sleep(1);
   }
   if (connected = 0) {
@@ -189,10 +217,12 @@ void start()
   if ((fp = fopen(pidfile,"w"))!= NULL) {
     fprintf(fp,"%d\n",getpid());
     fclose(fp);
+  } else {
+    syslog(LOG_INFO,"Warning:  Unable to open pid file\n");
   }
 
   // signal handlers started...device connected, now run run run!!!
- 
+  listen();
 } 
 
 void syntax(int argc, char *argv[], int i)
@@ -202,12 +232,13 @@ void syntax(int argc, char *argv[], int i)
   else
     fprintf(stderr,"Unknown argument:  %s\n",argv[i]);
 
-  fprintf(stderr,"Syntax:  %s [-api device] [-pid pidfile] [-tag logtag] [-debug]\n",progname);
+  fprintf(stderr,"Syntax:  %s [-api device] [-pid pidfile] [-tag logtag] [-debug] -device device\n",progname);
   fprintf(stderr,"         -api device  - X10 device driver api file (default: %s)\n",defdevice);
   fprintf(stderr,"         -pid pidfile - File to write driver PID to (default: %s)\n",defpidfile);
   fprintf(stderr,"         -tag logtag  - Tag to be written to syslog (default: %s)\n",progname);
   fprintf(stderr,"         -timeout #   - Timeout while waiting for response from api (default: 10)\n");
   fprintf(stderr,"         -debug       - Turn on debug mode\n");
+  fprintf(stderr,"         -device	- transceiver device file\n");
 }
 
 untty()
@@ -226,8 +257,12 @@ untty()
 static sighandler_type reapchild()
 {
   int status;
-  dprintf("%s: reapchild()\n");
+  syslog(LOG_INFO,"%s(%s): killing all children\n",progname,logtag);
+  dprintf("%s: reapchild()\n",progname);
+  kill(xmitpid,SIGHUP);
   while(waitpid(-1,&status,WNOHANG) > 0);
+  free(xmitstack);
+  xmitstack = NULL;
 }
 
 static sighandler_type die(int sig)
@@ -240,11 +275,242 @@ static sighandler_type die(int sig)
     dprintf("%s(%s): exiting on signal %d\n",progname,logtag,sig);
     errno = 0;
   }
+  syslog(LOG_INFO, "%s(%s): killing all children\n",progname,logtag);
+  dprintf("%s(%s): killing all children\n",progname,logtag);
+  if (xmitpid > 0)
+    kill(xmitpid,SIGHUP);
+  if (xmitstack != NULL){
+    free(xmitstack);
+    xmitstack = NULL;
+  }
   syslog(LOG_INFO, "%s(%s): closing all files and exiting\n",progname,logtag);
   dprintf("%s(%s): closing all files and exiting\n",progname,logtag);
   close(api);
   unlink(pidfile);
   closelog();
   exit(0);
+}
+
+static int tty_received(int hc, int uc, int fc, unsigned char *buf, int len)
+{
+  return(update_state(0,hc,uc,fc,buf,len));
+}
+
+xcvrio_t xcvrio = {
+  received:	tty_received,	// callback function for transmitter to call when data is available
+};
+
+static void listen()
+{
+  int n;
+  x10_message_t m;
+
+  sem_init(&start.lock,1,1);
+  xcvrio.device = serial;
+  xcvrio.debug = debug;
+  xcvrio.logtag = logtag;
+  xmitstack = (void **)malloc(1024*16);
+  xmitpid = clone((int (*)(void *))xmit_init,xmitstack,CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_PTRACE | CLONE_VM,(void *)&xcvrio);
+  if (xmitpid < 0) {
+    syslog(LOG_INFO, "%s(%s): unable to clone, exiting - %s\n",progname,logtag,strerror(errno));
+    unlink(pidfile);
+    closelog();
+    free(xmitstack);
+    exit(-1);
+  }
+
+  // physical device transmitter started...now listen to the api
+  while (1) {
+    memset(&m,0,sizeof(m));
+    n = read(api,&m,sizeof(m));
+    if (n < 0) {
+      syslog(LOG_INFO,"%s(%s): Error reading API interface - %s\n",progname,logtag,strerror(errno));
+      dprintf("%s:  Error reading API interface, continuing...\n");
+      continue;
+    }
+    if (n != sizeof(m)) {
+      syslog(LOG_INFO,"%s(%s): Error - incorrect API byte count, got %d, should have gotten %d\n",progname,logtag,n,sizeof(m));
+      dprintf("%s: Error - incorrect API byte count, got %d, should have gotten %d\n",progname,n,sizeof(m));
+      continue;
+    }
+    dprintf("X10 message: src=0x%x, hc=0x%x, uc=0x%x, cmd=0x%x, f=0x%x\n",m.source,m.housecode,m.unitcode,m.command,m.flag);
+    switch (m.source) {
+      case X10_API:
+        break;
+      case X10_DATA:
+      case X10_CONTROL:
+        xmit_send(m.housecode,m.unitcode,m.command);
+        update_state(1,m.housecode,m.unitcode,m.command,NULL,0);
+        break;
+      default:
+        dprintf("%s: Error - invalid API source %d\n",progname,m.source);
+        break;
+    }
+  } // while(1) loop
+}
+
+static int updatestatus(int hc, int uc, int value)
+{
+  x10_message_t m;
+  memset(&m,0,sizeof(m));
+  m.source = X10_DATA;
+  m.housecode = hc;
+  m.unitcode = uc;
+  m.command = value;
+  if (write(api,&m,sizeof(m)) == sizeof(m)){
+    state.uc[hc][uc] = value;
+    return 0;
+  }
+  else{
+    syslog(LOG_INFO,"%s(%s): Error - write did not complete\n",progname,logtag);
+    dprintf("%s: Error - write did not complete, incorrect byte count\n",progname);
+    return -1;
+  }
+}
+
+static void clear_uc(int hc)
+{
+  int i;
+  for (i = 0; i < MAX_UNITS; i++)
+    state.uc[hc][i] = 0;
+}
+
+unsigned char preset_low[16]= {19,23,13,16,26,29,32,35,45,48,39,42,0,3,6,10};
+unsigned char preset_high[16]={71,74,65,68,77,81,84,87,97,100,90,94,52,55,58,61};
+
+static int update_state(int dir, int hc, int uc, int fc,unsigned char *buf, int len)
+{
+  int i,ret=0;
+
+  dprintf("dir=%d, hc=0x%x, uc=0x%x, fc=0x%x\n",dir,hc,uc,fc);
+  hc &= 0x0f;
+  uc &= 0x0f;
+
+  sem_wait(&state.lock);
+
+  // if we get a preset dim high/low, we can store the level only if:
+  //   the previous housecode was stored
+  //   the previous message received was not a function (had to be unit)
+  //   a valid unit is stored
+  if ((fc == X10_CMD_PRESETDIMHIGH || fc == X10_CMD_PRESETDIMLOW)
+      && (state.hc>=0 && state.hc < MAX_HOUSECODES)
+      && (state.fcsent[state.hc] != 1)
+      && (state.lastuc != -1)) {
+    updatestatus(state.hc,state.lastuc,fc == X10_CMD_PRESETDIMLOW ? preset_low[hc] : preset_high[hc]);
+  }
+  if (hc != state.hc) {    // restart if new housecode received
+    state.hc = hc;
+    state.lastuc = -1;
+    state.fc = -1;
+  }
+  if (uc >= 0 && uc < MAX_UNITS) {
+    if (state.fcsent[state.hc]) {
+      clear_uc(state.hc);
+      state.fcsent[state.hc] = 0;
+    }
+    state.uc[state.hc][uc] = 1;
+    state.lastuc = uc;
+  }
+  log_store(dir, hc, uc, fc);
+  // something just changed so now update the queue head to wake everyone up
+
+  if (fc >= 0) {
+    // we have a completed command
+    // we may never have received a unit code so it may still be -1
+//              if (fc != X10_CMD_STATUS)
+      state.fcsent[state.hc] = 1;
+    state.fc = fc;
+    switch (fc) {
+    case X10_CMD_ALLLIGHTSOFF:      // we don't know which ones are lights
+    case X10_CMD_ALLUNITSOFF:
+      for (i = 0; i < MAX_HOUSECODES; i++) {
+        updatestatus(state.hc,i,0);
+      }
+      break;
+    case X10_CMD_ALLLIGHTSON:
+      for (i = 0; i < MAX_HOUSECODES; i++) {
+        updatestatus(state.hc,i,100);
+      }
+      break;
+    case X10_CMD_ON:
+      for (i = 0; i < MAX_UNITS; i++)
+        if (state.uc[state.hc][i]) {
+          updatestatus(state.hc,i,100);
+        }
+      break;
+    case X10_CMD_OFF:
+      for (i = 0; i < MAX_UNITS; i++)
+        if (state.uc[hc][i]) {
+          updatestatus(state.hc,i,0);
+        }
+      break;
+    case X10_CMD_DIM:
+      for (i = 0; i < MAX_UNITS; i++)
+        if (state.uc[hc][i]) {
+          int value = state.status[state.hc][i];
+          value -= 6;
+          if (value < 0)
+            value = 0;
+          updatestatus(state.hc,i,value);
+        }
+      break;
+    case X10_CMD_BRIGHT:
+      for (i = 0; i < MAX_UNITS; i++)
+        if (state.uc[hc][i]) {
+          int value = state.status[state.hc][i];
+          value += 6;
+          if (value > 100)
+            value = 100;
+          updatestatus(state.hc,i,value);
+        }
+      break;
+    case X10_CMD_STATUS:
+      dbg("status request to housecode %c",'A'+state.hc);
+      break;
+    case X10_CMD_STATUSOFF:
+      dbg("status of %c%d is OFF",'A'+state.hc,state.lastuc+1);
+      if (state.hc < MAX_HOUSECODES && state.lastuc < MAX_UNITS) {
+        updatestatus(state.hc,state.lastuc,0);
+      }
+      break;
+    case X10_CMD_STATUSON:
+      dbg("status of %c%d is ON",'A'+state.hc,state.lastuc+1);
+      if (state.hc < MAX_HOUSECODES && state.lastuc < MAX_UNITS) {
+        updatestatus(state.hc,state.lastuc,100);
+      }
+      break;
+    case X10_CMD_HAILREQUEST:
+      dbg("hail request to housecode %c",'A'+state.hc);
+      break;
+    case X10_CMD_HAILACKNOWLEDGE:
+      dbg("hail ACK to housecode %c",'A'+state.hc);
+      break;
+    case X10_CMD_EXTENDEDCODE:      // extended code
+      dbg("extended code to housecode %c",'A'+state.hc);
+      if (len > 0){
+        dbg("%s",dumphex(buf,len));
+        edata_store(state.hc,buf,len);
+      }
+      break;
+    case X10_CMD_EXTENDEDDATAA:     // extended data (analog)
+      dbg("extended data (analog) to housecode %c", 'A' + state.hc);
+      if (len > 0){
+        dbg("%s",dumphex(buf,len));
+        edata_store(state.hc,buf,len);
+      }
+      break;
+    case X10_CMD_PRESETDIMHIGH:     // already handled
+      dbg("preset dim low %c%d",'A'+state.hc,state.lastuc);
+      break;
+    case X10_CMD_PRESETDIMLOW:      // already handled
+      dbg("preset dim low %c%d",'A'+state.hc,state.lastuc);
+      break;
+    default:
+      ret = -1;
+      break;
+    }  // end of switch statement
+  } // end of if (fc >= 0)
+  sem_post(&state.lock);
+  return ret;
 }
 
