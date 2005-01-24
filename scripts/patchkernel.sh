56106d3e99e1d542de0dbab99c5a5716a6b47e13
whiles <empty>
2005-01-24 01:14:59 +0000

    fixed stuff

--- scripts/patchkernel.sh
+++ scripts/patchkernel.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #
+echo ""
+echo ""
+echo ""
+echo "executing:  $0 $*"
 currentdir=`pwd`
 cd /usr/src/linux-`uname -r`
 text=`grep x10 drivers/char/Makefile`
@@ -10,9 +14,21 @@ then
 else
 	echo "Kernel already patched..."
 fi
+if [ ! -d drivers/char/x10 ]
+then
+	echo "creating `pwd`/drivers/char/x10"
+	mkdir drivers/char/x10
+else
+	echo "`pwd`/drivers/char/x10 already exists...good"
+fi
 echo "Copying source code for drivers..."
+echo "cp -f $currentdir/dev/*.[ch] drivers/char/x10/"
 cp -f $currentdir/dev/*.[ch] drivers/char/x10/
+echo "cp -f $currentdir/dev/`uname -r | cut -c 1-3`/Makefile drivers/char/x10/"
 cp -f $currentdir/dev/`uname -r | cut -c 1-3`/Makefile drivers/char/x10/
+echo "done."
+echo ""
+echo ""
 echo "**********************************************************"
 echo "Kernel patched.  Make sure to turn on the X10 module in "
 echo "the kernel make options (make menuconfig or make xconfig)"
