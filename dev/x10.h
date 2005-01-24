febbafe7e9d04095a7c92ccf815fcf04575c14f3
whiles <empty>
2005-01-24 01:09:12 +0000

    added GNU license

--- dev/x10.h
+++ dev/x10.h
@@ -1,5 +1,24 @@
 #ifndef __X10_H__
 #define __X10_H__
+/*
+   This module is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as publised by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This module is distributed WITHOUT ANY WARRANTY; without even
+   the implied warranty of MERCHANTABILITY or FITNESS FOR A
+   PARTICULAR PURPOSE.  See the GNU Library General Public License
+   for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+   MA 02111-1307, USA
+
+   Should you need to contact the author, you can do so by email to
+   <wsh@sprintmail.com>.
+*/
 
 #include <linux/types.h>
 
