9ddb00e6d487d82eb1a059b265fe90ef91b0e04d
John Ralls <jralls@ceridwen.us>
2015-09-25 15:20:50 -0700

    Fix make.sh so that it isn't as finicky about version numbers.

--- dev/make.sh
+++ dev/make.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 #
-cp -f `uname -r | cut -d '.' -f 1,2`/Makefile .
+DIR=2.4
+VERSION=`uname -r | cut -d '.' -f 1,2`
+MAJOR=`echo $VERSION | cut -d '.' -f 1`
+MINOR=`echo $VERSION | cut -d '.' -f 2`
+if [ $MAJOR -ge 3 -a $MINOR -ge 10 ]; then
+  DIR=3.16
+elif [ $MAJOR -ge 3 -o $MAJOR -eq 2 -a $MINOR -ge 6 ]; then
+  DIR=2.6
+fi
+
+cp $DIR/Makefile .
 make $*
 rm -f Makefile
